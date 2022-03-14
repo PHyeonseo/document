6379852a09be25e8cf5770f5eca7a0502cb794fc
PHyeonseo <95402034+PHyeonseo@users.noreply.github.com>
2022-03-14 17:08:44 +0900

    Add files via upload

--- js/all_layout01.js
+++ js/all_layout01.js
@@ -3,9 +3,16 @@ $(function() {
 
   $('.mainSlide').slick({
     arrows:false,
-    autoplay:false,
+    autoplay:true,
     autoplaySpeed:2000,
     dots:true
+  });
+
+  $('.mainVisual .sildeAngle i:first-child').on('click',function() {
+    $('.mainSlide').slick('slickPrev')
+  });
+  $('.mainVisual .sildeAngle i:last-child').on('click',function() {
+    $('.mainSlide').slick('slickNext')
   });
 
 
