d404986f1b7c4aa870eea1c1cfcc422465f5fe3a
PHyeonseo <95402034+PHyeonseo@users.noreply.github.com>
2022-03-18 09:06:55 +0900

    Add files via upload

--- js/all_layout07.js
+++ js/all_layout07.js
@@ -1,31 +1,23 @@
-$(function() {
-/////////////////////////////////
+$(function () {
+  /////////////////////////////////
 
-//topBanner : park 2022.03.17
+  //topBanner : park 2022.03.17
 
-function topBannerHandle() {
-  // $('.topBanner').hide(); //display:none;
-  $('.topBanner').slideUp();
-}
-$('.topBanner .container i ').on('click',topBannerHandle)
-
-
-
-$('.visualSilde').slick( {
-  arrows:false,
-  autoplay:true,
-  autoplaySpeed:3000,
-  dots:true  //슬라이드 숫자
-});
+  function topBannerHandle() {
+    // $('.topBanner').hide(); //display:none;
+    $('.topBanner').slideUp();
+  }
+  $('.topBanner .container i ').on('click', topBannerHandle)
 
 
 
+  $('.visualSilde').slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true //슬라이드 숫자
+  });
 
 
-
-
-
-
-
-/////////////////////////////////
+  /////////////////////////////////
 })
