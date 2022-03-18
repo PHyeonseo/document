487eeeb8efac4cca0691bfced02aafafe49e77af
PHyeonseo <95402034+PHyeonseo@users.noreply.github.com>
2022-03-18 15:19:25 +0900

    Add files via upload

--- js/all_layout07.js
+++ js/all_layout07.js
@@ -12,11 +12,39 @@ $(function () {
 
 
   $('.visualSilde').slick({
-    arrows: false,
+    arrows: true,
     autoplay: true,
     autoplaySpeed: 3000,
-    dots: true //슬라이드 숫자
+    dots: true, //슬라이드 숫자
+    pauseOnHover: false,
+    prevArrow: '<div class="msLeft"></div>',
+    nextArrow: '<div class="msRight"></div>'
   });
+
+
+  $('.visualSlide figure').eq(1).addClass('oo');
+  $('.visualSilde').on('afterChange', function (e,s,c){
+    $('.visualSlide figure').eq(c+1).addClass('oo').siblings().removeClass('oo');
+  })
+
+  // eventProduct : park 2022.03.18 최종수정
+  $('.eProductSlide').slick({
+    arrows:false,
+    autoplay: true,
+  })
+
+  // allProduct : park 2022.03.18 최종수정
+  $('.aproductSlide').slick({
+    arrows:false,
+    dots: true,
+    slidesToShow: 5 //슬라이드 보여주는 개수
+  })
+  $('.allProduct i:first-child').on('click',function() {
+    $('.aproductSlide').slick('slickPrev');
+  })
+  $('.allProduct i:last-child').on('click',function() {
+    $('.aproductSlide').slick('slickNext');
+  })
 
 
   /////////////////////////////////
