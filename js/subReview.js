b9ee2f75572b582962fbe5f0067343bca05ce07d
PHyeonseo <95402034+PHyeonseo@users.noreply.github.com>
2022-03-23 17:49:33 +0900

    Add files via upload

---
+++ js/subReview.js
@@ -0,0 +1,13 @@
+  $(function () {
+
+
+    //topBanner : park 
+
+    function topBannerHandle() {
+      // $('.topBanner').hide(); //display:none;
+      $('.topBanner').slideUp();
+    }
+    $('.topBanner .container i ').on('click', topBannerHandle)
+
+
+  })
